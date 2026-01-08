cb7d2a07500c68c95b9da18879be6d550456ec1c
Chandler Hill <64368915+Channndo@users.noreply.github.com>
2026-01-07 23:20:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // After you deploy the Apps Script Web App,
 // paste the Web App URL here:
-window.ACA_API_URL = "https://script.google.com/macros/s/AKfycbxcFbxLJAFvnkXufw65bJbzzcd9z9DCq1-jF7LG5EURo3gogfO7c1yi-EE6bJF3GvB6OA/exec";
+window.ACA_API_URL = "https://script.google.com/macros/s/AKfycbxGRp8ONTS6N3aM0_0VCd20qf7P8mU4EVRagZ6JL1Astu_p76v2ZpyTqeQjykldr0A46g/exec";
 
 (function(){
   const form = document.getElementById('leadForm');
